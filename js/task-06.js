9ac7454c7e61c4d88f0080ad3bc9a9eebe7035df
Volodymyr <mv.design8@gmail.com>
2023-02-27 22:00:10 +0200

    task_6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector("#validation-input");
+
+inputEl.addEventListener("blur", (e) => {
+  if (e.target.value.length <= 6) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
+  
+});
+
+
