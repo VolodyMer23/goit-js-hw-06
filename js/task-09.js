9f956d9c853a0da245a5855f26bb5dff51d2b17d
Volodymyr <mv.design8@gmail.com>
2023-02-27 22:29:14 +0200

    task_9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorValueEl = document.querySelector('.color');
+const colorChangerBtn = document.querySelector('.change-color');
+
+colorChangerBtn.addEventListener('click', () => {
+  colorValueEl.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+  document.body.style.transition = 'background-color 450ms';
+})
