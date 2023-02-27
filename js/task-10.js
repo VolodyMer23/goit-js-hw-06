31fa4c567b69ba86b7e78d776ad91a554cc5e534
Volodymyr <mv.design8@gmail.com>
2023-02-28 00:35:16 +0200

    Task_10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  boxesContainerEl: document.querySelector("#boxes"),
+  controlFormEl: document.querySelector("#controls"),
+  quantInputEl: document.querySelector('#controls input'),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+}; 
+
+refs.boxesContainerEl.style.display = "flex";
+refs.boxesContainerEl.style.justifyContent = "left";
+refs.boxesContainerEl.style.gap = "5px";
+refs.boxesContainerEl.style.flexWrap = "wrap";
+
+let inputValue = 0;
+
+refs.quantInputEl.addEventListener("blur", (e) => {
+  inputValue = e.currentTarget.value;
+});
+
+refs.createBtn.addEventListener("click", createBoxes);
+refs.destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  let boxWidth = 30;
+  for (let i = 0; i < inputValue; i += 1) {
+    let string = `<div style='width: ${boxWidth}px; height: ${boxWidth}px; background-color: ${getRandomHexColor()};'></div>`;
+    boxWidth += 10;
+    refs.boxesContainerEl.insertAdjacentHTML("beforeend", string);
+  }
+}
+
+function destroyBoxes() {
+  refs.boxesContainerEl.innerHTML = "";
+}
