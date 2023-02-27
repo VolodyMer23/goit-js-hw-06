6088d259ed36773ed2789a9fae6b5126a3ee99d2
Volodymyr <mv.design8@gmail.com>
2023-02-27 22:17:15 +0200

    task_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password }
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Та цеж не діло, заповніть всі поля :)");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  e.currentTarget.reset();
+}
