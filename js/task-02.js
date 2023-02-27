5fb322917ca3fe9eadace80cca476482e41abc1b
Volodymyr <mv.design8@gmail.com>
2023-02-27 15:32:32 +0200

    task_2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredListEl = document.querySelector("#ingredients");
+
+const items = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("list");
+  const itemTitleEl = document.createElement("p");
+  itemTitleEl.textContent = ingredient;
+  itemEl.appendChild(itemTitleEl);
+  return itemEl;
+});
+
+ingredListEl.append(...items);
